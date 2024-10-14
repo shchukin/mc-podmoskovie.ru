f744edaed960e355111a868236751b88cf26944c
Anton Shchukin <a.a.shchukin@gmail.com>
2024-10-14 09:02:32 +0700

    form

--- scripts/anton-update.js
+++ scripts/anton-update.js
@@ -169,6 +169,162 @@ document.addEventListener("DOMContentLoaded", (event) => {
         });
     });
 
+    /* Форма */
 
+    /* Уведомления */
+    document.querySelectorAll('.alert').forEach(function (alert) {
+        alert.addEventListener('click', function () {
+            alert.style.display = 'none';
+        });
+    });
+
+    document.addEventListener('keyup', function (event) {
+        if (event.keyCode === 27) {
+            document.querySelectorAll('.alert').forEach(function (alert) {
+                alert.style.display = 'none';
+            });
+        }
+    });
+
+
+    /* Сама форма */
+    document.querySelectorAll('[data-js-form]').forEach(function (form) {
+        const subscriptionInputs = form.querySelectorAll('.input, .choice');
+        const subscriptionSubmit = form.querySelector('[data-js-submit]');
+        const subscriptionSuccessAlert = document.querySelector('[data-js-alert-success]');
+        const subscriptionFailureAlert = document.querySelector('[data-js-alert-failure]');
+
+        /* Состояния инпутов (на время отправки формы инпуты должны блокироваться) */
+        function disableSubscriptionInputs() {
+            subscriptionInputs.forEach((input) => {
+                input.querySelector('.input__widget, .choice__widget').setAttribute('disabled', 'disabled');
+            });
+        }
+
+        function enableSubscriptionInputs() {
+            subscriptionInputs.forEach((input) => {
+                input.querySelector('.input__widget, .choice__widget').removeAttribute('disabled');
+            });
+        }
+
+        /* Очистка инпутов (после успешной отправки форму должны очищаться) */
+        function cleanSubscriptionInputs() {
+
+            subscriptionInputs.forEach((input) => {
+
+                /* Есть три варианта инпутов: */
+
+                // 1) Текстовые инпуты внутри компонента .input (это всё, что не тег <select>):
+                const textInputs = input.querySelector('.input__widget:not(select)');
+                if (textInputs) {
+                    textInputs.value = '';
+                }
+
+                // 2) <select> внутри .input
+                const selects = input.querySelector('select.input__widget');
+                if (selects) {
+                    const placeholderOption = Array.from(selects.options).find(option => option.getAttribute('value') === 'placeholder');
+                    if (placeholderOption) {
+                        selects.value = placeholderOption.value;
+                        input.classList.add('input--placeholder-is-chosen');
+                    }
+                }
+
+                // 3) .choice__widget
+                const choices = input.querySelector('.choice__widget');
+                if (choices) {
+                    choices.checked = false;
+                }
+            });
+        }
+
+        /* Состояния кнопки */
+        function changeSubmitStateToLoading() {
+            subscriptionSubmit.classList.add('button--loading');
+            subscriptionSubmit.setAttribute('disabled', 'disabled');
+        }
+
+        function changeSubmitStateToSuccess() {
+            subscriptionSubmit.classList.remove('button--loading');
+            subscriptionSubmit.classList.add('button--success');
+            subscriptionSubmit.setAttribute('disabled', 'disabled');
+        }
+
+        function changeSubmitStateToFailure() {
+            subscriptionSubmit.classList.remove('button--loading');
+            subscriptionSubmit.classList.add('button--warning');
+            subscriptionSubmit.setAttribute('disabled', 'disabled');
+        }
+
+        function changeSubmitStateToPristine() {
+            subscriptionSubmit.classList.remove('button--loading', 'button--success', 'button--warning');
+            subscriptionSubmit.removeAttribute('disabled');
+        }
+
+        /* Если пользователь начал взаимодействовать с инпутами, то убираем уведомления с прошлой попытки отправки: */
+        subscriptionInputs.forEach(input => input.addEventListener('input', () => {
+            subscriptionFailureAlert.style.display = 'none';
+        }));
+
+
+        /* Отправка */
+        form.addEventListener('submit', function (event) {
+            event.preventDefault();
+
+            /* Если с прошлой попытки висит уведомление об ошибке: */
+            subscriptionFailureAlert.style.display = 'none';
+
+            /* Начинаем отправку данных, для начала блокируем форму */
+            disableSubscriptionInputs();
+            changeSubmitStateToLoading();
+
+            /* Представим, что 3000ms отправляем данные */
+            setTimeout(function () {
+
+                /* ... дальше развилка, пусть для примера будет рандом 50/50: */
+
+                // Если данные успешно отправлены
+                if (Math.random() < 0.5) {
+
+                    // показываем зелёное уведомление:
+                    subscriptionSuccessAlert.style.display = 'block';
+
+                    // показываем галочку на кнопке:
+                    changeSubmitStateToSuccess();
+
+                    // и то и другое на 4.5 секунды:
+                    setTimeout(function () {
+                        subscriptionSuccessAlert.style.display = 'none';
+                        changeSubmitStateToPristine();
+                        enableSubscriptionInputs();
+
+                        //и очищаем форму:
+                        cleanSubscriptionInputs();
+                    }, 4500);
+
+                }
+
+                // Если произошла ошибка
+                else {
+
+                    // показываем красное уведомление
+                    subscriptionFailureAlert.style.display = 'block';
+
+                    // Показываем восклицательный знак на кнопке:
+                    changeSubmitStateToFailure();
+
+                    // В данном случае всего 2 секунды, чтобы пользователь мог быстро вернуться к работе с формой.
+                    // Уведомление в этом случае НЕ убираем -- пусть висит, пока пользователь не увидит и явно не закроет, или не начнёт заново заполнять форму / попытается отправить:
+                    setTimeout(function () {
+                        changeSubmitStateToPristine();
+                        enableSubscriptionInputs();
+                    }, 2000);
+
+                }
+
+            }, 3000);
+
+        });
+    });
 
 });
